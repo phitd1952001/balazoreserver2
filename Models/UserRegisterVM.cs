a851f7fba60bb1c3061f97e01abf7a37498b9cb5
Đường Phi <phi1952001@gmail.com>
2025-09-24 23:43:55 +0700

    done Register page and done function Validation

--- Models/UserRegisterVM.cs
+++ Models/UserRegisterVM.cs
@@ -1,11 +1,24 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public class UserRegisterVM 
 {
-    public string UserName { get; set; } = "phi";
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public bool Gender { get; set; } = false;
-    public string City { get; set; }
-    public List<string> Hobbies { get; set; } = new List<string>() {"Ngủ" };
+    [Required(ErrorMessage = "Username không được để trống")]
+    [MinLength(3, ErrorMessage = "Username phải có ít nhất 3 ký tự")]
+    //maxLength
+    [MaxLength(20, ErrorMessage = "Username không được vượt quá 20 ký tự")]
+    public string UserName { get; set; } = "";
+
+    [Required(ErrorMessage = "Email không được để trống")]
+    [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Password không được để trống")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số")]
+    public string Password { get; set; } = "";
+    public bool Gender { get; set; } = false; // true false
+
+    [Required(ErrorMessage = "City không được để trống")]
+    public string City { get; set; } = "";
+    public List<string> Hobbies { get; set; } = new List<string>();
 }
